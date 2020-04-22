f16ff9925750d763de6d77f9098b3ed37e209dfe
Jaime <jaime.cruz.hostalot@gmail.com>
2020-04-22 15:54:34 +0200

    [TEST] added user to be able to create a comment, not authorized yet

--- spec/requests/api/comment/create_spec.rb
+++ spec/requests/api/comment/create_spec.rb
@@ -1,6 +1,10 @@
 RSpec.describe 'GET /api/comments', type: :request do 
 
-  let!(:article){create(:article)}
+  let!(:user) { create(:user)}
+  let(:user_credentials) { user.create_new_auth_token }
+  let(:user_headers)  {{HTTP_ACCEPT: 'application/json'}.merge!(user_credentials)}
+
+  let!(:article) { create(:article)}
 
   describe 'successfully' do
     before do
@@ -9,6 +13,7 @@ RSpec.describe 'GET /api/comments', type: :request do
         body: 'whello there people!',
         article_id: article.id
        }
+       headers: user_headers
     end
 
     it 'returns a 200 response' do
