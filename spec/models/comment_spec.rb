b2d265539dea85eea0863d8015a88b7e94f43ad7
Jaime <jaime.cruz.hostalot@gmail.com>
2020-04-22 14:24:23 +0200

    [Assocciations] Comment belongs to article

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,7 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
+
   describe "Comment has db columns" do
     it { is_expected.to have_db_column :body }
+  end  
+
+  describe "Assocciations" do
+    it { is_expected.to belong_to :article }
   end  
 end
